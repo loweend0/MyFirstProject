2856aa35bbde440164019b426fbac810d5092238
loweend0 <111999220+loweend0@users.noreply.github.com>
2022-08-29 12:55:59 -0500

    Add project files.

---
+++ MyFirstProject/main.cpp
@@ -0,0 +1,18 @@
+/*
+Drake Lowe
+C++ Fall 2022
+Aug 31, 2022
+Lab 1 Introduction to C++ Programming
+This lab is a demo of my ability to work with an IED
+*/
+
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello World!" << "Blue";
+
+
+	return 0;
+}
